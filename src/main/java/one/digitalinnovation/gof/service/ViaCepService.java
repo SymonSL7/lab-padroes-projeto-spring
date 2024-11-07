d23fb5ef1680c6eb6f623582eac2bab5020f257d
Symon Lima <symonlima07@gmail.com>
2024-11-07 12:13:36 -0300

    Retirando importações desnecessárias no ViaCepService

--- src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -3,8 +3,7 @@ package one.digitalinnovation.gof.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import one.digitalinnovation.gof.model.Endereco;
 
